d993baf2edbbe67366e75d5b2c7e46d143398759
Андрей <andrepq71q@gmail.com>
2025-01-16 13:49:56 +0300

    sending_for_verification

--- module_9/module_9_creating_functions_fly.py
+++ module_9/module_9_creating_functions_fly.py
@@ -3,8 +3,9 @@ import random
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
 
-result = list(map(lambda f,s: f == s, first, second))
+result = list(map(lambda f, s: f == s, first, second))
 print(result)
+
 
 def get_advanced_writer(file_name):
     def write_everything(*data_set):
@@ -14,16 +15,15 @@ def get_advanced_writer(file_name):
 
     return write_everything
 
+
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
-
 
 
 class MysticBall:
 
     def __init__(self, *worlds):
         self.worlds = worlds
-
 
     def __call__(self):
         return random.choice(self.worlds)
